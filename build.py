e699158724bdc9de24dca36cae1ad9c1b06fbe63
Md. Minhazul Haque <mdminhazulhaque@gmail.com>
2024-12-17 02:45:01 +0600

    automatic github repo fetch and build site

---
+++ build.py
@@ -0,0 +1,13 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("index.html")
+
+with open("data.json", "r") as fp:
+    projects = json.load(fp)
+
+content = template.render(projects=projects)
+
+with open("./public/index.html", "w") as html:
+    html.write(content)
---
+++ data.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import os
+
+owner = "mdminhazulhaque"
+
+projects = [
+    "alias-generator",
+    "awesome-bangla-parenting",
+    "aws-cli-cheatsheet",
+    "aws-stale-dns-finder",
+    "bitbucket-cli",
+    "django-baby-log",
+    "gcloud-cli-cheatsheet",
+    "probhat-osx",
+    "probhat.im",
+    "ruet-thesis-template-latex",
+    "ssh-tunnel-manager",
+    "traefik-converter",
+]
+
+token = os.getenv("GITHUB_TOKEN")
+
+headers = {"Authorization": f"Bearer {token}"}
+
+data = []
+
+for p in projects:
+    print("🚀", p)
+
+    query = f"""{{
+        repository(owner: "{owner}", name: "{p}")
+        {{
+            openGraphImageUrl
+        }}
+    }}"""
+
+    item = {
+        "name": p
+    }
+
+    try:
+        response = requests.post("https://api.github.com/graphql", json={'query': query}, headers=headers)
+        item["image"] = response.json()["data"]["repository"]["openGraphImageUrl"]
+    except:
+        pass
+
+    try:
+        response = requests.get(f"https://api.github.com/repos/{owner}/{p}/pages", headers=headers)
+        item["url"] = response.json()["html_url"]
+    except:
+        pass
+
+    try:
+        response = requests.get(f"https://api.github.com/repos/{owner}/{p}", headers=headers)
+        item["description"] = response.json()["description"]
+    except:
+        pass
+
+    data.append(item)
+
+with open("data.json", "w") as fp:
+    json.dump(data, fp, indent=2)
